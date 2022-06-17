6ede72b635725c21a1ef80ba5669a86efc428369
フナクラベ <k020g1164@g.neec.ac.jp>
2022-06-17 23:31:44 +0900

    使わなくなったシェーダーを全削除。

--- Resource/ShaderFiles/GSInstanceVS.hlsl
+++
@@ -1,9 +0,0 @@
-#include "GSINstanceShaderHeader.hlsli"
-
-VSOutput main(float4 pos : POSITION, uint instanceID : SV_InstanceID)
-{
-    VSOutput output;
-    output.pos = pos; //À•W‚És—ñ‚ğæZ
-    output.id = instanceID;
-    return output;
-}
--- Resource/ShaderFiles/ObjectGetShadowMapPS.hlsl
+++
@@ -1,9 +0,0 @@
-#include "ObjectGetShadowMapShaderHeader.hlsli"
-
-float4 main(VSOutput input) : SV_TARGET
-{
-
-    // シャドウマップにZ値を書き込む。
-    return float4(input.svpos.z, input.svpos.z, input.svpos.z, 1.0f);
-
-}
