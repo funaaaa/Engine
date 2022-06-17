0a5e036b685e08cf0d50b2314fa7b392b173122e
フナクラベ <k020g1164@g.neec.ac.jp>
2022-06-17 23:18:50 +0900

    Engine.hを削除とJoyconの処理をすべてコメントアウト

--- Src/Engine/Engine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "DirectXBase.h"
-//#include "ObjectManager.h"
-#include "SoundManager.h"
-#include "PiplineManager.h"
-#include "Joycon.h"
-#include "DebugText.h"
-#include "RenderTarget.h"
-#include "MultiPathFunction.h"
