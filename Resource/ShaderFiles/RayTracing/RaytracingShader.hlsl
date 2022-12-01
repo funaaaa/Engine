6ee031ab83d35371d7136a32fe50aa25efc94b5b
フナクラベ <k020g1164@g.neec.ac.jp>
2022-12-02 03:27:31 +0900

    タイヤ痕復活。

--- Resource/ShaderFiles/RayTracing/RaytracingShader.hlsl
+++ Resource/ShaderFiles/RayTracing/RaytracingShader.hlsl
@@ -316,7 +316,7 @@ bool ProcessingBeforeLighting(inout Payload PayloadData, Vertex Vtx, MyAttribute
     }
     
     // InstanceIDがCHS_IDENTIFICATION_INSTANCE_DEF_GI_TIREMASKだったらテクスチャに色を加算。
-    if (InstanceID == CHS_IDENTIFICATION_INSTANCE_DEF_GI_TIREMASK || InstanceID == CHS_IDENTIFICATION_INSTANCE_DEF_TIREMASK)
+    if (InstanceID == CHS_IDENTIFICATION_INSTANCE_DEF_GI_TIREMASK || InstanceID == CHS_IDENTIFICATION_INSTANCE_DEF_TIREMASK || InstanceID == CHS_IDENTIFICATION_INSTANCE_DEF_TIREMASK_AO)
     {
         float4 tiremasktex = (float4) tireMaskTexture[uint2((uint) (Vtx.subUV.x * 4096.0f), (uint) (Vtx.subUV.y * 4096.0f))];
         TexColor += tiremasktex * tiremasktex.a;
